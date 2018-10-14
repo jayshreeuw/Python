c53646f3c22e6b4f343380ec0d97df57d0176ab3
jayshreeuw <jayawasnik@gmail.com>
2018-10-13 17:38:16 -0700

    Count_method

---
+++ DataTypes/List/Count_method.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+aCool_List = ["superman", "spiderman", 1947, 1987, "Spiderman"]
+
+oneMoreList = [22,34,56,34,34,78,98]
+
+#Trying to find the number of occurences in a variable:
+
+#Method : Count(x)
+
+print(aCool_List.count(22)) #prints 0 because 22 is not present in aCool_List
+
+print(aCool_List.count('spiderman'))
+
+print(oneMoreList.count(34))
