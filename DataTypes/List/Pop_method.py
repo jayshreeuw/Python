48c9fbd59aad7fc93d91cacc04ef70008e5b2b60
jayshreeuw <jayawasnik@gmail.com>
2018-10-13 18:35:50 -0700

    pop method

---
+++ DataTypes/List/Pop_method.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+aCool_List = ["superman", "spiderman", 1947, 1987, "Spiderman"]
+
+oneMoreList = [22,34,56,34,34,78,98]
+
+#deleting values
+
+aCool_List.pop(1)
+
+print(aCool_List)
+
+#Without index using pop method:
+aCool_List.pop()
+print(aCool_List)
+
+'''
+list.pop([i]):list.pop()
+Remove the item at the given position in the list and return it
+'''
