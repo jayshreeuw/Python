264f2075446cce44529ca616b0d805e7f325aeed
jayshreeuw <jayawasnik@gmail.com>
2018-10-22 22:20:32 -0700

    loops

---
+++ Loops/for_continue.py
@@ -0,0 +1,12 @@
+var=input("Enter a string: ")
+
+for letter in var:
+
+    if(letter==' '):
+        #continue
+        break
+        print("Condition Met")
+    else:
+        print(letter)
+
+print("I am outside of For Loop")
